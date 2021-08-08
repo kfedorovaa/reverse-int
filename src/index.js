8320a6558144993ddad4340b676c69a082b9ba6f
Kate Fedorova <ekf5va@gmail.com>
2021-08-08 11:02:36 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let reversed = 0;
+  n=Math.abs(n);
+  while (n>0) {
+      reversed = reversed * 10 + n % 10;
+      n = Math.floor(n / 10);
+  }
+  return reversed;
+
 }
+
